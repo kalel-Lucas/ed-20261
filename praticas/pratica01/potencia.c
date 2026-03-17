f0fdf8c40f0b9368e41351a6d038f74524f1ff94
Kalel Lucas <kalellucas21@gmail.com>
2026-03-16 21:20:57 -0300

    Refazendo algumas partes do exercício de potencia

--- praticas/pratica01/potencia.c
+++ praticas/pratica01/potencia.c
@@ -9,20 +9,5 @@
     }
 
     int main () {
-        int num_expo;
-        float num_base; 
-        float resultado_potencia; 
-        while (1) {
-            printf("\n\tPor favor, me mande o numero da base:\n");
-            scanf("%f",&num_base);
-            printf("\n\tAgora, um numero natural diferente de zero para o expoente:\n");
-            scanf("%d",&num_expo);
-            if (num_expo<0) {
-                printf("\n\tNumero de expoente invalido!");
-            }
-            else if (num_expo>0) {
-                resultado_potencia = calcula_potencia(num_base,num_expo);
-                printf("\n\tResultado: %.1f",resultado_potencia);
-            }
-        }
+      
     }
