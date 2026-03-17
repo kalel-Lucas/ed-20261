76e3ec9a0a23c28668bc85d411152e1685ae03e5
Kalel Lucas <kalellucas21@gmail.com>
2026-03-16 22:01:20 -0300

    Potencia.c concluída usando a metodologia de algoritmos!

--- praticas/pratica01/potencia.c
+++ praticas/pratica01/potencia.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-    float calcula_potencia (float base, float expo) {
-        float potencia_temp = 1; 
+    void calcula_potencia (float base, float expo) {
+        if (expo<=0) {
+            printf("\n\tPor favor, digite um expoente inteiro positivo!");
+        }
+        else if (expo>0) {
+            int potencia_temp = 1; 
         for (int i = 0; i<expo; i++) {
             potencia_temp = potencia_temp*base; 
         }
-            return potencia_temp; 
+        printf("\n\tResultado: %d",potencia_temp);
     }
+}   
 
     int main () {
-      
+      calcula_potencia(2,4);
+         calcula_potencia(4,4);
+          calcula_potencia(1,1);
+            calcula_potencia(2,0);
+                calcula_potencia(10,3);
     }
